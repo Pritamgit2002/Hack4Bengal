a237149b20725d6741603b1ffd4b7fa6a4fc2335
swarnadeep <aritrasarkar2002@gmail.com>
2023-07-10 16:12:01 +0530

    logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from 'react';
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
-import Logo from '../public/assets/logo.png'
+import Logo from '../public/assets/Logo.png'
 // import { useRouter } from 'next/router';
 
 const Navbar = () => {
